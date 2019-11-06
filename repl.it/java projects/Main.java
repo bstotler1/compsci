5d1703f75eddee9cf76a0ba5b24ceeca6cf39f86
bstotler1 <maniac4wheeler@gmail.com>
2019-11-06 14:49:08 -0500

    Add files via upload

---
+++ repl.it/java projects/Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    Movie m = new Movie();
+    m.seeMovie(3.50, 2);
+    m.seeMovie(4.99, 0);
+    m.seeMovie(18.0, 4);
+    m.seeMovie(12.0, 3);
+    m.seeMovie(13.50, 5);
+    m.seeMovie(12.0, 5);
+    m.seeMovie(7.25, 5);
+    m.seeMovie(11.59, 5);
+  }
+}
