2947b5d84b055cb5d790ac0a80ac553585b595a4
bstotler1 <maniac4wheeler@gmail.com>
2019-11-06 12:43:39 -0500

    Create Main.java

---
+++ repl.it/java projects/If Statements #1 Part 2/Password/Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    Password w = new Password();
+    w.Pass("password");
+    w.Pass("Password");
+    w.Pass("Midlothian");
+    w.Pass("Football");
+  }
+}
