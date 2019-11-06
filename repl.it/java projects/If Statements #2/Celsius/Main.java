a511655c4e7cf6566b47be7b167e2f670234a11d
bstotler1 <maniac4wheeler@gmail.com>
2019-11-06 12:50:22 -0500

    Create Main.java

---
+++ repl.it/java projects/If Statements #2/Celsius/Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    Celsius c = new Celsius();
+    System.out.println(c.Temperature(32));
+    System.out.println(c.Temperature(-2));
+  }
+}
