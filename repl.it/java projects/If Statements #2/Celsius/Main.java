aff318551029f06d1313396e49c1d0865290b4f2
bstotler1 <maniac4wheeler@gmail.com>
2019-11-07 21:56:17 -0500

    a

--- repl.it/java projects/If Statements #2/Celsius/Main.java
+++
@@ -1,7 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    Celsius c = new Celsius();
-    System.out.println(c.Temperature(32));
-    System.out.println(c.Temperature(-2));
-  }
-}
