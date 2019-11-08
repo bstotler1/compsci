c616b47c42fb08de337e913f91f89ca69a42520a
bstotler1 <maniac4wheeler@gmail.com>
2019-11-08 02:45:16 +0000

    a

--- repl.it/java projects/If Statements #1 Part 2/Positive Negative/Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    PositiveNegative posneg = new PositiveNegative();
-    posneg.posNegative(6);
-    posneg.posNegative(0);
-    posneg.posNegative(-12);
-  }
-}
