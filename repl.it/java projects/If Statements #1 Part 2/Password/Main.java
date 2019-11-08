9541f7354ef1515e550c151c0f36261f27527c03
bstotler1 <maniac4wheeler@gmail.com>
2019-11-08 02:40:05 +0000

    a

--- repl.it/java projects/If Statements #1 Part 2/Password/Main.java
+++
@@ -1,9 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    Password w = new Password();
-    w.Pass("password");
-    w.Pass("Password");
-    w.Pass("Midlothian");
-    w.Pass("Football");
-  }
-}
--- repl.it/java projects/If Statements #1 Part 2/Weather/Main.java
+++
@@ -1,9 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    Weather t = new Weather();
-    t.Temperature(67);
-    t.Temperature(90);
-    t.Temperature(78);
-    t.Temperature(43);
-  }
-}
