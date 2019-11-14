a216ff9fe4224d0a42bf9d142fe965515a932923
EC2 Default User <ec2-user@ip-172-31-51-172.ec2.internal>
2019-11-14 19:44:56 +0000

    Kroger

---
+++ Java Programs/While Loop #1/Kroger/Kroger.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Kroger{
+    int stock, amount;
+    public Kroger(){};
+    Scanner input = new Scanner(System.in);
+    public void buy(){
+        System.out.print("Enter the amount of stock of your item at Kroger: ");
+        stock = input.nextInt();
+        while(stock!=0){
+            System.out.print("Enter the amount you want to buy of your item at Kroger: ");
+            amount = input.nextInt();
+            stock -= amount;
+            System.out.println("You now have " + stock + " at Kroger");
+        }
+        System.out.println("You have ran out of your item at Kroger!");
+    }
+}
